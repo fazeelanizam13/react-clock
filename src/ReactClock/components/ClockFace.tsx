33796ea4ab44c0cd64cb8adfae151608874462d5
Fazeela <fazeelanizam13@gmail.com>
2024-04-20 11:51:42 +0530

    fix import

--- src/ReactClock/components/ClockFace.tsx
+++ src/ReactClock/components/ClockFace.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { ReactClockProps } from '..';
+import { ReactClockProps } from '../index';
 
 type ClockFaceProps = {
   size: ReactClockProps['size'];
